829ad5feae503466377c7dcd2d800dc58a9fa1a8
OlgaVasilevna <56765282+OlgaVasilevna@users.noreply.github.com>
2021-01-18 19:57:41 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <h1>Welcome to REACT!</h1>
+            <h1>Welcome to REACT!!!</h1>
         )
     }
 }
